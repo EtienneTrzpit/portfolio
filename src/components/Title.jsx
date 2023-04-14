cc22a914ddf2ce8af28d21dd2c718153659a2e4a
EtienneTrzpit <etienne.trzpit@free.fr>
2023-04-14 17:20:07 +0200

    3 boutons

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,6 +4,8 @@ import { Container } from '@mui/system';
 import { Toolbar } from '@mui/material';
 import Button from '@mui/material/Button';
 import InfoIcon from '@mui/icons-material/Info';
+import SchoolIcon from '@mui/icons-material/School';
+import MailIcon from '@mui/icons-material/Mail';
 import React from 'react';
 
 const Title = () => {
@@ -11,7 +13,11 @@ const Title = () => {
         <AppBar>
             <Toolbar>
                 <Container maxWidth="sm">
-                    <Button variant="contained" startIcon={<InfoIcon/>}>About</Button>
+                    <div className='container-xxl d-flex justify-content-center'>
+                        <Button variant="contained" startIcon={<InfoIcon/>}>À propos</Button>
+                        <Button variant="contained" startIcon={<SchoolIcon/>}>Savoir-faire</Button>
+                        <Button variant="contained" startIcon={<MailIcon/>}>Contact</Button>
+                    </div>
                     <Typography variant="h1">Etienne Trzpit</Typography>
                     <Typography variant="h2">Développeur web</Typography>
                 </Container>
