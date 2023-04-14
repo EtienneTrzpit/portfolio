1a46b6176b32728b5c81281a1e7a73f8aec8fba5
EtienneTrzpit <etienne.trzpit@free.fr>
2023-04-14 19:04:55 +0200

    création stepper

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import NavBar from './components/NavBar';
 import CardTitle from './components/CardTitle';
+import Stepper from './components/Stepper';
 
 const App = () => {
     return (
         <div>
             <NavBar />
             <CardTitle />
+            <Stepper />
         </div>
     );
 }
