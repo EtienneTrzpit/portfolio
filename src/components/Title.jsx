73c7786d606c1fc42fed7744ca67f6cbc954a9c1
EtienneTrzpit <etienne.trzpit@free.fr>
2023-04-14 16:54:55 +0200

    bouton about

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,21 @@
 import  {Typography}  from '@mui/material';
 import {AppBar} from '@mui/material';
+import { Container } from '@mui/system';
+import { Toolbar } from '@mui/material';
+import Button from '@mui/material/Button';
+import InfoIcon from '@mui/icons-material/Info';
 import React from 'react';
 
 const Title = () => {
     return (
         <AppBar>
-            <Typography variant="h1">Etienne Trzpit</Typography>
-            <Typography variant="h2">Développeur web</Typography>
+            <Toolbar>
+                <Container maxWidth="sm">
+                    <Button variant="contained" startIcon={<InfoIcon/>}>About</Button>
+                    <Typography variant="h1">Etienne Trzpit</Typography>
+                    <Typography variant="h2">Développeur web</Typography>
+                </Container>
+            </Toolbar>
         </AppBar>
     );
 }
